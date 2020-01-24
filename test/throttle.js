8b1ff7222f9b1674717a670a882b650ca3e4932c
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2020-01-24 22:13:07 +0530

    updating

--- test/throttle.js
+++ test/throttle.js
@@ -1,3 +1,54 @@
 var pot = require('pot');
 
-pot.throttlit('accounts', 'contacts');
+var domain = 'accounts';
+var model = 'contacts';
+
+pot.throttlit('accounts', 'contacts', {
+  apis: {
+    confirm: {
+      second: 0,
+      day: 1,
+      month: 2
+    },
+    verify: {
+      second: 0,
+      day: 1,
+      month: 2
+    }
+  },
+  ips: {
+    confirm: {
+      second: 0,
+      minute: 1,
+      hour: 2,
+      day: 3
+    },
+    verify: {
+      second: 0,
+      minute: 1,
+      hour: 2,
+      day: 3
+    }
+  }
+}, {
+  confirm: {
+    POST: function (i) {
+      return {
+        url: pot.resolve(domain, '/apis/v/' + model + '/dummy'),
+        headers: {
+          'X-Action': 'confirm'
+        }
+      }
+    }
+  },
+  verify: {
+    POST: function (i) {
+      return {
+        url: pot.resolve(domain, '/apis/v/' + model + '/dummy'),
+        headers: {
+          'X-Action': 'verify'
+        }
+      }
+    }
+  }
+});
